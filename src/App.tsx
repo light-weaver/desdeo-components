378587c547b7491051f7804c1aef9fb5c2b5e803
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2021-04-01 17:08:28 +0300

    Properly defined variables in the function rendering the bar charts as useStates and added them to the dependency list of useEffects.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React, { useRef, useEffect, useState } from "react";
-import { select, selection, Selection, pointer } from "d3-selection";
+import { select, Selection, pointer } from "d3-selection";
 import { scaleLinear, scaleBand } from "d3-scale";
-import { max, min } from "d3-array";
-import { axisLeft, axisBottom } from "d3-axis";
-import randomstring from "randomstring";
+import { axisBottom } from "d3-axis";
 import "d3-transition";
-import { easeElastic } from "d3-ease";
-import { appendFile } from "node:fs";
+import { easeCubic } from "d3-ease";
 
 const initialData = [
   {
@@ -27,6 +24,18 @@ const initialData = [
     value: -500,
     selected: false,
   },
+  {
+    name: "efficiency",
+    direction: "max",
+    value: 25000,
+    selected: false,
+  },
+  {
+    name: "???",
+    direction: "max",
+    value: 200,
+    selected: false,
+  },
 ];
 
 const dimensions = {
@@ -38,10 +47,18 @@ const dimensions = {
   chartWidth: 500,
 };
 
-const ideal = [25, 0.95, -871];
-const nadir = [101, 0.11, 801];
+const ideal = [25, 0.95, -871, 150000, 300];
+const nadir = [101, 0.11, 801, 520, 100];
 
 function App() {
+  /* 
+  These hooks are used to store states, which are meaningful to the operation of this component.
+  - ref should only ever point to the single svg-element
+  - selection is used to have a usable hook to get hold of the DOM through d3 throughout this
+    function
+  - data is used to refer to the data being displayed
+  - prefPointLocs refers to the locations of the pointers indicating preference on each horizontal bar
+   */
   const ref = useRef<SVGSVGElement | null>(null);
   const [selection, setSelection] = useState<null | Selection<
     SVGSVGElement | null,
@@ -49,66 +66,84 @@ function App() {
     null,
     undefined
   >>(null);
-  const [data, setData] = useState(initialData);
+  const [data] = useState(initialData);
   const [prefPointerLocs, setPrefPointerLocs] = useState(
     data.map((d) => d.value)
   );
 
-  const xs = data.map((d, i) => {
-    return scaleLinear()
-      .domain([nadir[i], ideal[i]])
-      .range([0, dimensions.chartWidth]);
-  });
+  // create an array of linear scales to scale each objective being maximized
+  const [xs] = useState(
+    data.map((d, i) => {
+      return scaleLinear()
+        .domain([nadir[i], ideal[i]])
+        .range([0, dimensions.chartWidth]);
+    })
+  );
 
-  const xs_rev = data.map((d, i) => {
-    return scaleLinear()
-      .domain([ideal[i], nadir[i]])
-      .range([0, dimensions.chartWidth]);
-  });
+  // create also an array of reverse scales to be used when minimizing
+  const [xs_rev] = useState(
+    data.map((d, i) => {
+      return scaleLinear()
+        .domain([ideal[i], nadir[i]])
+        .range([0, dimensions.chartWidth]);
+    })
+  );
 
-  const y = scaleBand()
-    .domain(data.map((d) => d.name))
-    .range([0, dimensions.chartHeight])
-    .padding(0.35);
+  // create an array of bottom axised to work as the individual x-axis for each bar
+  const [xAxises] = useState(
+    data.map((d, i) => {
+      if (d.direction === "max") {
+        return axisBottom(xs[i]);
+      } else {
+        return axisBottom(xs_rev[i]);
+      }
+    })
+  );
 
-  const xAxises = data.map((d, i) => {
-    if (d.direction === "max") {
-      return axisBottom(xs[i]).ticks(10);
-    } else {
-      return axisBottom(xs_rev[i]);
-    }
-  });
-
+  // This is the main use effect and should really be fired only once per render.
   useEffect(() => {
+    // create a discrete band to position each of the horizontal bars
+    // this has to be created in the effect because it's type cannot be coerced as of yet
+    // (may in a future version of d3)
+    const y = scaleBand()
+      .domain(data.map((d) => d.name))
+      .range([0, dimensions.chartHeight])
+      .padding(0.35);
+
     if (!selection) {
       setSelection(select(ref.current));
     } else {
-      // Axixes
-      const xAxisGroup = data.map((d, i) =>
+      // Position the x-axises
+      data.map((d, i) =>
         selection
           .append("g")
           .attr(
             "transform",
             `translate(${dimensions.marginLeft}, ${
-              y(d.name)! + y.bandwidth() * 1.05
+              y.call(y, d.name)! + y.bandwidth() * 1.05
             })`
           )
           .call(xAxises[i])
-          .append("text")
-          .attr("fill", "black")
-          .attr(
-            "transform",
-            `translate(${dimensions.chartWidth / 2}, ${y.bandwidth() * 0.3})`
-          )
-          .text(`${d.name}`)
-          .attr("font-size", 12)
-          .attr("font-weight", "bold")
       );
 
-      // positive space for max, negative space for min
+      // enter selection, append to this
       const enter = selection.append("g").selectAll("rect").data(data).enter();
 
-      // max pos space
+      // append the labels for each bar positioned after the bar, roughly center to the bar
+      enter
+        .append("text")
+        .attr("fill", "black")
+        .attr("text-anchor", "start")
+        .attr("transform", (d) => {
+          return `translate(${
+            dimensions.marginLeft + dimensions.chartWidth + 10
+          }, ${y(d.name)! + y.bandwidth() / 2})`;
+        })
+        .text((d) => `${d.name}(${d.direction})`)
+        .attr("font-size", 18)
+        .attr("font-weight", "bold");
+
+      // draw the positive space for max problems and negative space for min problems
       enter
         .append("rect")
         .attr("transform", `translate(${dimensions.marginLeft}, 0)`)
@@ -132,7 +167,7 @@ function App() {
           }
         });
 
-      // neg pos space
+      // draw the positive space for min problems and the negative space for pos problems
       enter
         .append("rect")
         .attr("transform", `translate(${dimensions.marginLeft}, 0)`)
@@ -162,7 +197,7 @@ function App() {
           }
         });
 
-      // overlay to handle events
+      // draw a transparent overlay on top of each bar to work as an event detector
       enter
         .append("rect")
         .attr("class", "preferencePointerOverlay")
@@ -174,43 +209,33 @@ function App() {
         .attr("x", 0)
         .attr("height", y.bandwidth)
         .attr("fill", "yellow")
-        .attr("opacity", 0.0)
-        .on("click", (event, d) => {
-          const match_index = data.findIndex((datum) => datum.name === d.name);
-          const prefValue = (d.direction === "max"
-            ? xs[match_index]
-            : xs_rev[match_index]
-          ).invert(pointer(event)[0]);
-          // SUPER IMPORTANT TO **NOT** CHANGE STATE, BUT TO CREATE A NEW OBJECT!
-          const newLocs = prefPointerLocs.map((loc) => loc);
-          newLocs[match_index] = prefValue;
-          setPrefPointerLocs(newLocs.map((loc) => loc));
-        });
+        .attr("opacity", 0.0);
 
+      // draw the preference pointers on each bar on top of the event overlay
       enter
         .append("line")
         .attr("class", "preferencePointer")
+        .attr("y1", (d) => y(d.name)!)
+        .attr("y2", (d) => y(d.name)! + y.bandwidth())
         .attr("x1", (d, i) => {
           if (d.direction === "max") {
-            return xs[i](prefPointerLocs[i]) + dimensions.marginLeft;
+            return xs[i](d.value) + dimensions.marginLeft;
           } else {
-            return xs_rev[i](prefPointerLocs[i]) + dimensions.marginLeft;
+            return xs_rev[i](d.value) + dimensions.marginLeft;
           }
         })
         .attr("x2", (d, i) => {
           if (d.direction === "max") {
-            return xs[i](prefPointerLocs[i]) + dimensions.marginLeft;
+            return xs[i](d.value) + dimensions.marginLeft;
           } else {
-            return xs_rev[i](prefPointerLocs[i]) + dimensions.marginLeft;
+            return xs_rev[i](d.value) + dimensions.marginLeft;
           }
         })
-        .attr("y1", (d) => y(d.name)!)
-        .attr("y2", (d) => y(d.name)! + y.bandwidth())
-        .attr("stroke-width", 5)
-        .attr("stroke", "blue")
+        .attr("stroke-width", 3)
+        .attr("stroke", "black")
         .attr("fill", "none");
     }
-  }, [selection]);
+  }, [selection, data, xs, xs_rev, xAxises]);
 
   /* We need an useEffect to watch for changes in the preference pointer values and update not
    *  just the location of the pointers, but also the event handlers on the overlay.
@@ -219,9 +244,14 @@ function App() {
     if (!selection) {
       return;
     } else {
-      const enter = selection.selectAll(".preferencePointer").data(data);
+      // select the preference pointers
+      const enterPointer = selection.selectAll(".preferencePointer").data(data);
 
-      enter
+      // update the x location of the preference pointer according to the state of prefPointerLocs
+      enterPointer
+        .transition()
+        .duration(300)
+        .ease(easeCubic)
         .attr("x1", (d, i) => {
           if (d.direction === "max") {
             return xs[i](prefPointerLocs[i]) + dimensions.marginLeft;
@@ -235,16 +265,15 @@ function App() {
           } else {
             return xs_rev[i](prefPointerLocs[i]) + dimensions.marginLeft;
           }
-        })
-        .attr("y1", (d) => y(d.name)!)
-        .attr("y2", (d) => y(d.name)! + y.bandwidth())
-        .attr("stroke-width", 5)
-        .attr("stroke", "blue")
-        .attr("fill", "none");
-      const enter_2 = selection
+        });
+
+      // select the event detection overlay, this needs to be updated because the first useEffect
+      // does not have access to the updated state of preferencePointerLocs
+      const enterOverlay = selection
         .selectAll(".preferencePointerOverlay")
         .data(data);
-      enter_2.on("click", (event, d) => {
+
+      enterOverlay.on("click", (event, d) => {
         const match_index = data.findIndex((datum) => datum.name === d.name);
         const prefValue = (d.direction === "max"
           ? xs[match_index]
@@ -256,14 +285,14 @@ function App() {
         setPrefPointerLocs(newLocs.map((loc) => loc));
       });
     }
-  }, [prefPointerLocs]);
+  }, [selection, prefPointerLocs, data, xs, xs_rev, xAxises]);
 
   return (
     <div>
       <svg ref={ref} width={dimensions.width} height={dimensions.height}></svg>
       <ul>
         {prefPointerLocs.map((d, i) => (
-          <li>{`${data[i].name}(${data[i].direction}) = ${d}`}</li>
+          <li key={i}>{`${data[i].name}(${data[i].direction}) = ${d}`}</li>
         ))}
       </ul>
     </div>
