41f6a88ff4914053b35c062dab77ed8bec18c5c1
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2021-04-09 13:36:13 +0300

    Added new example data with 4 objectives and 10 objective vectors.

--- src/data/ExampleData.tsx
+++ src/data/ExampleData.tsx
@@ -1,6 +1,6 @@
 import { ObjectiveData } from "../types/ProblemTypes";
 
-const exampleDataSingle5Objectives: ObjectiveData = {
+export const exampleDataSingle5Objectives: ObjectiveData = {
   values: [
     {
       selected: false,
@@ -13,7 +13,7 @@ const exampleDataSingle5Objectives: ObjectiveData = {
   nadir: [101, 0.11, 801, 520, 100],
 };
 
-const exampleDataSingle3Objectives: ObjectiveData = {
+export const exampleDataSingle3Objectives: ObjectiveData = {
   values: [
     {
       selected: false,
@@ -26,4 +26,51 @@ const exampleDataSingle3Objectives: ObjectiveData = {
   nadir: [101, 0.11, 801],
 };
 
-export { exampleDataSingle5Objectives, exampleDataSingle3Objectives };
+export const exampleDataTen4Objectives: ObjectiveData = {
+  names: ["Price", "Quality", "Time", "Efficiency"],
+  directions: [1, -1, 1, -1],
+  ideal: [25, 0.95, -871, 10],
+  nadir: [101, 0.11, 801, 0],
+  values: [
+    {
+      selected: false,
+      value: [50, 0.2, -500, 1],
+    },
+    {
+      selected: false,
+      value: [20, 0.9, -300, 2],
+    },
+    {
+      selected: false,
+      value: [90, 0.01, -800, 3],
+    },
+    {
+      selected: false,
+      value: [40, 0.3, 500, 4],
+    },
+    {
+      selected: false,
+      value: [55, 0.6, -550, 5],
+    },
+    {
+      selected: false,
+      value: [66, 0.22, 602, 6],
+    },
+    {
+      selected: false,
+      value: [26, 0.18, 100, 7],
+    },
+    {
+      selected: false,
+      value: [99, 0.33, 0, 8],
+    },
+    {
+      selected: false,
+      value: [80, 0.73, -100, 9],
+    },
+    {
+      selected: false,
+      value: [33.3, 0.81, -150, 10],
+    },
+  ],
+};
