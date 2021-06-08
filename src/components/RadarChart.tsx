e9913c8a99fa076b2961054db2c6e5e01ed07ea7
jpajasmaa <juuso.pajasmaa@gmail.com>
2021-06-08 09:03:38 +0300

    Draws correctly with fake data, also mouse inputs

--- src/components/RadarChart.tsx
+++ src/components/RadarChart.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useCallback, useRef } from "react";
-import { select, Selection, pointer } from "d3-selection";
+import { select, selectAll, Selection, pointer } from "d3-selection";
 import { scaleLinear, scaleBand, scaleOrdinal } from "d3-scale";
 import { line, lineRadial, curveLinearClosed } from "d3-shape";
 import { axisBottom, axisLeft } from "d3-axis";
@@ -25,6 +25,33 @@ const defaultDimensions = {
   marginBottom: 30,
 };
 
+const exampleData = [
+  [
+    //iPhone
+    { axis: "Battery Life", value: 0.22 },
+    { axis: "Brand", value: 0.28 },
+    { axis: "Contract Cost", value: 0.29 },
+    { axis: "Design And Quality", value: 0.17 },
+    { axis: "Have Internet Connectivity", value: 0.22 },
+  ],
+  [
+    //Samsung
+    { axis: "Battery Life", value: 0.27 },
+    { axis: "Brand", value: 0.16 },
+    { axis: "Contract Cost", value: 0.35 },
+    { axis: "Design And Quality", value: 0.13 },
+    { axis: "Have Internet Connectivity", value: 0.2 },
+  ],
+  [
+    //Nokia Smartphone
+    { axis: "Battery Life", value: 0.26 },
+    { axis: "Brand", value: 0.1 },
+    { axis: "Contract Cost", value: 0.3 },
+    { axis: "Design And Quality", value: 0.14 },
+    { axis: "Have Internet Connectivity", value: 0.22 },
+  ],
+];
+
 export const RadarChart = ({
   objectiveData,
   dimensionsMaybe,
@@ -39,73 +66,30 @@ export const RadarChart = ({
   const [dimensions] = useState(
     dimensionsMaybe ? dimensionsMaybe : defaultDimensions
   );
-  const [data, SetData] = useState(objectiveData);
+  const [real_data, SetData2] = useState(objectiveData);
 
-  useEffect(() => {
-    SetData(objectiveData);
-  }, [objectiveData]);
+  const [data, SetData] = useState(exampleData);
 
-  //const datadims = data.values[0].value.length; // get number of objectives. todo: make a getter
-  //console.log("dims", datadims)
-  //const colors = ["#EDC951","#CC333F","#00A0B0"]
-
-  const colorScale = scaleOrdinal().range(["#EDC951", "#CC333F", "#00A0B0"]);
-
-  const allAxis = data.names;
-  //["Price", "Quality", "Time"] // data.names; //(data.names.map((name) => name )) fix this
-  const axisNumbers = allAxis.length;
-  const radius = Math.min(dimensions.chartWidth, dimensions.chartHeight) / 2;
-  console.log("rad", radius);
-  //const Format = format('%');
-  
-  // temp constraints
-  let minDomain = 0;
-  let maxDomain = 1;
-  data.names.map((_,i) => {
-    minDomain =  data.ideal[i] < data.nadir[i] ? data.ideal[i] : data.nadir[i];
-    maxDomain = data.ideal[i] > data.nadir[i] ? data.ideal[i] : data.nadir[i];
-  })
-
-  const maxValue = maxDomain; // get this from the data
-
-  const rs = useCallback(() => {
-    return data.ideal.map((_, i) => {
-      return scaleLinear()
-        .domain([
-          data.ideal[i] < data.nadir[i] ? data.ideal[i] : data.nadir[i], // min
-          data.ideal[i] > data.nadir[i] ? data.ideal[i] : data.nadir[i], // max
-        ])
-        .range([0, radius]);
-    });
-  }, [data, maxValue]);
-
-  const yAxises = useCallback(() => {
-    return data.directions.map((_, i) => {
-      return axisLeft(rs()[i]);
-    });
-  }, [data, rs]);
-
-  const xs = useCallback(
-    () =>
-      scaleBand()
-        .domain(data.names.map((name) => name))
-        .range([0, 360])
-        .padding(1),
-    [dimensions, data]
-  );
-
-  const radScale = scaleLinear()
-  //.domain([minDomain, maxDomain])
-  .domain([minDomain,maxDomain])
-  .range([0, 360]);
-
-  let angle = (Math.PI * 2) / axisNumbers;
-  // add svg and update selection
   const renderH =
     dimensions.chartHeight + dimensions.marginBottom + dimensions.marginTop;
-
   const renderW =
-    dimensions.chartWidth + dimensions.marginLeft + dimensions.marginRight;
+    dimensions.chartWidth + dimensions.marginLeft + dimensions.marginRight; //If the supplied maxValue is smaller than the actual one, replace by the max in the data
+  let maxValue = 0.5; // get this from the real data
+  const colors = ["#EDC951", "#CC333F", "#00A0B0", "#AAAAAA", "#BBBBBB"];
+
+  // oma nootti: ei varmaan kannata olla xs koska pitäisi olla vain scalelinear joka skaalaa oikealle etäisyydelle origosta akselille
+  // pisteen ja radialScale joka kääntää kulman ja laittaa oikealle kohdalle ympyräkoordinaateissa.
+  // COmeon: tietysti käännät x arvon lineaarisesti skaalattuna sitten angleksi eli polaariseksi koordinaatiksi ja y arvon lineaarisesti skaalattuna radiukseski.
+
+  const allAxis = exampleData[0].map(function (i, _) {
+      return i.axis;
+    }), //Names of each axis
+    total = allAxis.length, //The number of different axes
+    radius = Math.min(renderW / 2, renderH / 2), //Radius of the outermost circle
+    angleSlice = (Math.PI * 2) / total; //The width in radians of each "slice"
+  const levels = 4; // how many background circles we want
+  // scale to linearize the data
+  const rScale = scaleLinear().range([0, radius]).domain([0, maxValue]);
 
   useEffect(() => {
     if (!selection) {
@@ -129,23 +113,25 @@ export const RadarChart = ({
       .append("g")
       .attr("transform", "translate(" + renderW / 2 + ", " + renderH / 2 + ")");
 
-    let axisGrid = g.append("g").attr("class", "axiswrap");
-    // draw the circles
+    // wrapper for grid and axises
+    const axisGrid = g.append("g").attr("class", "axisWrap");
+    // draw the background circles
     axisGrid
       .selectAll("circle")
-      .data(range(1, 4).reverse())
+      .data(range(1, levels).reverse())
       .enter()
       .append("circle")
+      .attr("class", "gridCircle")
       //.attr("transform", "translate(300,300)") // for this, do as in parallelaxes, scale with sizes
-      .attr("r", function (d, _) {
-        return (radius / 4) * d;
-      })
+      .attr("r", (d, _) => (radius / levels) * d)
       .style("fill", "white")
       .style("stroke", "lightblue")
-      .style("fill-opacity", 0);
+      .style("fill-opacity", 0.2);
+
+    // TODO: draw labels
 
     // draw the axes
-    let axis = axisGrid
+    const axis = axisGrid
       .selectAll(".axis")
       .data(allAxis)
       .enter()
@@ -156,81 +142,111 @@ export const RadarChart = ({
       .append("line")
       .attr("x1", 0)
       .attr("y1", 0)
-      .attr("x2", function (_, i) {
-        return radScale(maxValue * 1.1) * Math.cos(angle * i - Math.PI / 2);
-      }) // could make these own function, bc imo prettier/clearer that way
+      .attr(
+        "x2",
+        (_, i) => rScale(maxValue) * Math.cos(angleSlice * i - Math.PI / 2)
+      )
       .attr(
         "y2",
-        (_, i) => radScale(maxValue * 1.1) * Math.sin(angle * i - Math.PI / 2)
+        (_, i) => rScale(maxValue) * Math.sin(angleSlice * i - Math.PI / 2)
       )
       // .attr("transform", "translate(300,300)") // for this, do as in parallelaxes, scale with sizes
       .attr("class", "line")
       .style("stroke", "black")
-      .style("stroke-width", "2px");
+      .style("stroke-width", "5px");
+
     // axis labels
+    axis
+      .append("text")
+      .attr("class", "legend")
+      .style("font-size", "10px")
+      .attr("text-anchor", "middle")
+      .attr("dy", "0.35em")
+      .attr(
+        "x",
+        (_, i) =>
+          rScale(maxValue * 0.95) * Math.cos(angleSlice * i - Math.PI / 2)
+      )
+      .attr(
+        "y",
+        (_, i) =>
+          rScale(maxValue * 0.9) * Math.sin(angleSlice * i - Math.PI / 2)
+      )
+      .text((d) => d);
 
-    const linesData = data.values.map((datum) => {
-      return datum.value.map((v, i) => {
-        return [xs().call(xs, data.names[i])!, radScale(v)];
-      });
-    });
+    const radarLine = lineRadial()
+      .curve(curveLinearClosed)
+      // @ts-ignore
+      .radius((d) => rScale(d.value))
+      .angle((_, i) => i * angleSlice);
 
-    // This is a mess. TODO: make it again so that datapoints would actually hit the axels and then worry about moving them to the right positions.
-    //const lineRad = lineRadial().curve(curveLinearClosed).radius(d, 
-
-    console.log("linedata",linesData)
-    // also problem is that values don't touch the axels. Need to make that happen someway
-    //radScale(maxValue * 1.1) * Math.cos(angle * i - Math.PI / 2);
-    //  value: [50, 0.2, -500],
-    // bug here, otherwise should work. i goes over the index, making the path to undef.
-    let ist = 0
-    const lines2 = linesData.map((datum) => {
-      return lineRadial()
-        .curve(curveLinearClosed)
-        .radius((d, i) => {console.log("tämä",d, i, d[i]); return (radScale(d[i] * 1.1)*Math.cos(angle* i - Math.PI/2) ); })
-        //.radius(360)
-        .angle((_, i) => i * angle)(
-        datum.map((d) => {
-          console.log("i", ist)
-          ist += 1
-          return [d[0], d[1]];
-        })
-      );
-    });
-
-    console.log("l2",lines2)
-    const lines = linesData.map((datum) => {
-      return line()(
-        datum.map((d,i) => {
-          console.log("arvot", d, i)
-          console.log( "tulos", [(radScale(d[0]* 1.1)*Math.cos(angle*i - Math.PI/2)), (radScale(d[1] *1.1)*Math.sin(angle*i - Math.PI/2)) ]   )
-          return [d[0], d[1] ];
-        })
-      );
-    });
-
-    console.log("l",lines)
-    console.log("data values", data.values)
-
-    // create lines data
     //Create a wrapper for the blobs
     let blobWrapper = g
       .selectAll(".radarWrapper")
-      .data(data.values)
+      .data(exampleData) // this needs real data too
       .enter()
       .append("g")
       .attr("class", "radarWrapper");
 
-    // Needs data there to work and at right form.
+    // append the backgrounds from the solution points
+    blobWrapper
+      .append("path")
+      .attr("class", "radarArea")
+      // @ts-ignore
+      .attr("d", (d, _) => radarLine(d)) // from the example this should feed the datum for lines
+      // @ts-ignore
+      .attr("fill", (_, i) => colors[i])
+      .attr("fill-opacity", 0.5)
+      //.on('mouseover', ((d,i) => selectAll('.radarArea').transition().duration(200)).style('fill-opacity', 0.1));
+      // @ts-ignore
+      .on("mouseover", function (d, i) {
+        //Dim all blobs
+        selectAll(".radarArea")
+          .transition()
+          .duration(200)
+          .style("fill-opacity", 0.1);
+        //Bring back the hovered over blob
+        select(this).transition().duration(200).style("fill-opacity", 0.8);
+      })
+      .on("mouseout", function () {
+        //Bring back all blobs
+        selectAll(".radarArea")
+          .transition()
+          .duration(200)
+          .style("fill-opacity", 0.5);
+      });
+
+    // append the solution points
+    blobWrapper
+      .selectAll(".radarCicle")
+      .data((d, _) => d)
+      .enter()
+      .append("circle")
+      .attr("class", "radarCicle")
+      .attr("r", levels)
+      .attr(
+        "cx",
+        (d, i) => rScale(d.value) * Math.cos(angleSlice * i - Math.PI / 2)
+      )
+      .attr(
+        "cy",
+        (d, i) => rScale(d.value) * Math.sin(angleSlice * i - Math.PI / 2)
+      )
+      .style("fill", "yellow")
+      .style("fill-opacity", 0.8);
+
+    //Create the outlines
     blobWrapper
       .append("path")
       .attr("class", "radarStroke")
-      .attr("d", (_, i) => { console.log("viivat", i); return lines[i];} ) // from the example this should feed the datum for lines
-      .attr("stroke", "red")
-      .attr("stroke-width", "5px");
+      // @ts-ignore
+      .attr("d", (d, _) => radarLine(d))
+      .style("stroke-width", 2 + "px")
+      .style("stroke", (_, i) => colors[i])
+      .style("fill", "none");
 
     //selection.selectAll("g").attr("transform", `translate(0 ${dimensions.marginTop})`);
-  }, [selection, dimensions, data]); // add data and active one
+  }, [selection]); // add data and active one
 
   return <div ref={ref} id="container" className="svg-container"></div>;
 };
